2213d581b7a82101a994cdbb72ca5b1104ad8095
Ed Wei <ed@highfive.com>
2013-09-13 19:22:41 -0700

    Updated and working automated push script.

--- push_angularjs_release.sh
+++ push_angularjs_release.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ $# != 1 || $# != 2 ]; then
+  echo "Usage: $0 ANGULAR_JS_SRC_DIR [RELEASE_TAG(optional)]"
+  echo "  e.g. $0 ~/thirdparty-angularjs v1.2.0-rc.2"
+  echo "  e.g. $0 ~/thirdparty-angularjs"
+  echo " Remember to build angularjs before running this command."
+  exit 1
+fi
+
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 FILE_LIST=" \
@@ -11,20 +19,28 @@ FILE_LIST=" \
 ANGULAR_JS_SRC_DIR=$1/build
 RELEASE_TAG=$2
 
-pushd $ANGULAR_JS_SRC_DIR
-echo now in directory $(pwd)
-
-cp $FILE_LIST $SCRIPT_DIR
-cat > bower.json << _EOF
+(cd $ANGULAR_JS_SRC_DIR && cp $FILE_LIST $SCRIPT_DIR)
+if [ "x$RELEASE_TAG" != "x" ]; then
+  cat << ABCF > bower.json 
 {
   "name": "angular",
-  "version": "$RELEASE_TAG",
+  "version": "${RELEASE_TAG#v}",
   "main": "./angular.js",
   "dependencies": {
   }
 }
-_EOF
+ABCF
+fi
 
-#git commit -a
-#git tag
+echo Release tag ${RELEASE_TAG}
+git commit -a
+git diff HEAD^
+echo -n "Do you want to tag and push? (y/N)"
+read ANSWER
+if [ $ANSWER == "y" ]; then
+  if [ "x$RELEASE_TAG" != "x" ]; then
+    git tag ${RELEASE_TAG}
+  fi
+  git push origin $RELEASE_TAG
+fi
 
